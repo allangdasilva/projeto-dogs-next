c8e367ce02899561e51d730331b9c1156815bd82
Allan Silva <allandasilva33@outlook.com>
2025-05-20 20:58:16 -0300

    userContext criado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,30 @@ import "./globals.css";
 import { type_display } from "@/functions/fonts";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { UserContextProvider } from "@/context/user-context";
+import getUsuario from "@/actions/get-usuario";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
   description: "Rede social para cachorros.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data } = await getUsuario();
   return (
     <html lang="pt-br">
       <body className={type_display.variable}>
-        <div className="App">
-          <Header />
-          <main className="AppMain">{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={data}>
+          <div className="App">
+            <Header />
+            <main className="AppMain">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   );
